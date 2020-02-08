bf8f76c1e4dcfd0faf362024369c2ec03abbd632
newkritsada <newtronetic@gmail.com>
2020-02-09 02:42:36 +0700

    Update Repository

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+class AppContainer extends React.Component {
+    render(){
+        return (
+            <div>Hello Word</div>
+        );
+    }
+}
+
+export default AppContainer;
