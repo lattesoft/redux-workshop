3203029c34a44d50c548fb2084dc4582e9f487b0
Komphet Meesab <komphet.me@gmail.com>
2020-02-09 11:33:32 +0700

    Update Setup Next.js and Redux

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CounterComponent from '../components/CounterComponent';
+
+export default class Home extends React.Component {
+    render(){
+        return (
+            <CounterComponent/>
+        )
+    }
+}
